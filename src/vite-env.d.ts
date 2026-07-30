b8fee5ceeaeb7b764154b2ff7065d2351d4c7474
yahyakemalaksu <yahyakemalaksu@gmail.com>
2026-07-30 16:29:46 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,11 @@ declare module '*.png' {
   export default content;
 }
 
+declare module '*.PNG' {
+  const content: string;
+  export default content;
+}
+
 declare module '*.jpg' {
   const content: string;
   export default content;
